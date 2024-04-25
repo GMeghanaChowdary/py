95d9ba71991368538010450221608a0c57d215a2
GMeghanaChowdary <167081845+GMeghanaChowdary@users.noreply.github.com>
2024-04-25 09:33:10 +0530

    Create merge sort.py

---
+++ merge sort.py
@@ -0,0 +1,41 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        # Merge the two sorted halves
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Copy the remaining elements of left_half
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        # Copy the remaining elements of right_half
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Input
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Sort the list using Merge Sort
+merge_sort(arr)
+
+# Output
+print(" ".join(map(str, arr)))
